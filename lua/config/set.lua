b4703a23e026aaa50e11c7338f3b2ae5b1eebc47
chupack <datinik2019@gmail.com>
2025-08-01 21:03:24 +0400

    change border from rounded to none

--- lua/config/set.lua
+++ lua/config/set.lua
@@ -6,7 +6,7 @@ vim.cmd([[colorscheme gruvbox]])
 
 -- :help options
 vim.cmd(":hi statusline guibg=NONE")
-vim.o.winborder = "rounded"
+vim.o.winborder = "none"
 vim.opt.backup = false                          -- creates a backup file
 vim.opt.clipboard = "unnamedplus"               -- allows neovim to access the system clipboard
 vim.opt.cmdheight = 2                           -- more space in the neovim command line for displaying messages
